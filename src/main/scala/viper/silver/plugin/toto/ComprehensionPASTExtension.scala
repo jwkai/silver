44293a3375d22fbb1d759775d97f8b2604b64440
toto <tokaeo@cs.ubc.ca>
2023-07-19 00:15:05 -0700

    Edit typechecking for the comprehension PAst nodes

--- src/main/scala/viper/silver/plugin/toto/ComprehensionPASTExtension.scala
+++ src/main/scala/viper/silver/plugin/toto/ComprehensionPASTExtension.scala
@@ -1,6 +1,6 @@
 package viper.silver.plugin.toto;
 
-import viper.silver.ast.{Exp, Field, Position}
+import viper.silver.ast.{Exp, Field, NoPosition, Position}
 import viper.silver.parser.PExtender
 import viper.silver.parser._
 
@@ -10,42 +10,66 @@ import scala.xml.NodeSeq;
 case class PComprehension(op: PCall, unit: PExp, mappingFieldReceiver: PMappingFieldReceiver, filter: PExp)(val pos: (Position, Position)) extends PExtender with PExp {
   override val getSubnodes: Seq[PNode] = Seq(op, unit, mappingFieldReceiver, filter)
 
+//  override val typ =
+
   override def typecheck(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = {
+    var messagesOut : Seq[String] = Seq()
+    t.checkTopTyped(filter, None)
+//    PDomainType("Operator", Seq(PType))(pos)
     t.checkTopTyped(unit, None)
-    t.checkTopTyped(op, None)
+    val correctOpType = ComprehensionPlugin.makeDomainType("Operator", Seq(unit.typ))
+    t.checkTopTyped(op, Some(correctOpType))
+    val setType: PType = filter.typ match {
+      case PSetType(e) => e
+      case _ =>
+        messagesOut = messagesOut :+ "Filter should of Set[...] type."
+        return Some(messagesOut)
+    }
+    mappingFieldReceiver.typecheckComp(t, n, unit.typ, setType)
+//    mappingFieldReceiver.receiver.typ match {
+//      case PDomainType(domain, args) =>
+//        if (domain.name == "Receiver")
+//          ()
+//        else
+//          messagesOut = messagesOut :+ "Receiver expression not a receiver."
+//      case _ =>
+//        messagesOut = messagesOut :+ "Receiver expression not a receiver."
+//    }
+    this.typ = unit.typ
     None
   }
 
   override def typecheck(t: TypeChecker, n: NameAnalyser, expected: PType): Option[Seq[String]] = {
-    t.checkTopTyped(op, None)
+    t.checkTopTyped(this, Some(expected))
     None
   }
 
 
   val _typeSubstitutions: Seq[PTypeSubstitution] = Seq(PTypeSubstitution.id)
 
-  override def typeSubstitutions: collection.Seq[PTypeSubstitution] = _typeSubstitutions
+  override def typeSubstitutions: collection.Seq[PTypeSubstitution] = unit.typeSubstitutions
 
-  override def forceSubstitution(ts: PTypeSubstitution): Unit = ()
+  override def forceSubstitution(ts: PTypeSubstitution): Unit = unit.forceSubstitution(ts)
 
   override def translateExp(t: Translator): Exp = {
-    val translatedOp = t.exp(op)
-    val translatedUnit = t.exp(unit)
-    val translatedFilter = t.exp(filter)
-    receiver match {
-      case appliedMapping @ PCall(mapping, args, typeAnnotated) =>
-        args.head match {
-          case PFieldAccess(appliedReceiver, fieldIden) =>
-            Comprehension(
-              translatedOp,
-              translatedUnit,
-              Some(t.exp(appliedMapping.copy(args = args.tail)(appliedMapping.pos))),
-              t.exp(fieldIden), t.exp(appliedReceiver), translatedFilter)(t.liftPos(this))
-        }
-      case PFieldAccess(appliedReceiver, fieldIden) =>
-        Comprehension(translatedOp, translatedUnit, None,
-          t.exp(fieldIden), t.exp(appliedReceiver),translatedFilter)(t.liftPos(this))
-    }
+    t.exp(unit)
+//    val translatedOp = t.exp(op)
+//    val translatedUnit = t.exp(unit)
+//    val translatedFilter = t.exp(filter)
+//    receiver match {
+//      case appliedMapping @ PCall(mapping, args, typeAnnotated) =>
+//        args.head match {
+//          case PFieldAccess(appliedReceiver, fieldIden) =>
+//            Comprehension(
+//              translatedOp,
+//              translatedUnit,
+//              Some(t.exp(appliedMapping.copy(args = args.tail)(appliedMapping.pos))),
+//              t.exp(fieldIden), t.exp(appliedReceiver), translatedFilter)(t.liftPos(this))
+//        }
+//      case PFieldAccess(appliedReceiver, fieldIden) =>
+//        Comprehension(translatedOp, translatedUnit, None,
+//          t.exp(fieldIden), t.exp(appliedReceiver),translatedFilter)(t.liftPos(this))
+//    }
   }
 
 }
--- src/main/scala/viper/silver/plugin/toto/MappingFieldReceiverPAST.scala
+++ src/main/scala/viper/silver/plugin/toto/MappingFieldReceiverPAST.scala
@@ -1,39 +1,58 @@
 package viper.silver.plugin.toto
 
 import viper.silver.FastMessaging
-import viper.silver.ast.Position
-import viper.silver.parser.{NameAnalyser, PCall, PDomainType, PExp, PExtender, PField, PIdnUse, PNode, PType, TypeChecker}
+import viper.silver.ast.{NoPosition, Position}
+import viper.silver.parser.{NameAnalyser, PCall, PDomainFunction, PDomainType, PExp, PExtender, PField, PIdnUse, PNode, PType, PTypeSubstitution, TypeChecker}
 
 case class PMappingFieldReceiver(mapping: PCall, fieldID: PIdnUse, receiver: PCall)(val pos: (Position, Position))
   extends PExtender {
 
   override val getSubnodes: Seq[PNode] = Seq(mapping, fieldID, receiver)
 
-  override def typecheck(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = {
-    var messagesOut = Seq()
-    t.checkTopTyped(mapping, None)
-    t.checkTopTyped(receiver, None)
-    receiver.typ match {
-      case PDomainType(domain, args) =>
-        if (!(domain.name == "Receiver")) messagesOut ++= "Receiver expression not a receiver."
-      case _ =>
-        messagesOut ++= "Receiver expression not a receiver."
-    }
-    mapping.typ match {
-      case PDomainType(domain, args) =>
-        if (domain.name == "Mapping")
-          t.checkTopTyped(fieldID, Some(args.head))
-        else
-          messagesOut ++= "Mapping expression not a mapping."
-      case messagesOut ++= "Mapping expression not a Mapping."
-    }
-    //    t.checkInternal(fieldID)
-//    n.definition(null)(fieldID) match {
-//      case Some(value) => fieldID.decl = value
-//      case None => messagesOut ++= "Field in comprehension not found."
+  def typecheckComp(t: TypeChecker, n: NameAnalyser, typeUnit: PType, typeFilter: PType): Option[Seq[String]] = {
+    var messagesOut : Seq[String] = Seq()
+
+//    receiver.typeSubstitutions
+//    n.definition(null)(receiver.idnuse) match {
+//      case Some(PDomainFunction(idndef, formalArgs, typ, unique, interpretation)) => typ.substitute()
+//      case None => ???
 //    }
+
+//    receiver.typeSubstitutions = receiver.typeSubstitutions.addOne(PTypeSubstitution())
+//    PDomainType("Receiver",Seq())()
+    val correctReceiverType = ComprehensionPlugin.makeDomainType("Receiver", Seq(typeFilter))
+    t.checkTopTyped(receiver, Some(correctReceiverType))
+    t.checkTopTyped(fieldID, None)
+    val correctMappingType = ComprehensionPlugin.makeDomainType("Mapping",
+      Seq(fieldID.typ,typeUnit))
+    t.checkTopTyped(mapping, Some(correctMappingType))
+//    receiver.typ match {
+//      case PDomainType(domain, args) =>
+//        if (!(domain.name == "Receiver"))
+//          messagesOut = messagesOut :+ "Receiver expression not a receiver."
+//      case _ =>
+//        messagesOut = messagesOut :+ "Receiver expression not a receiver."
+//    }
+//    mapping.typ match {
+//      case PDomainType(domain, args) =>
+//        if (domain.name == "Mapping")
+//          t.checkTopTyped(fieldID, Some(args.head))
+//        else
+//          messagesOut = messagesOut :+ "Mapping expression not a mapping."
+//      case _ =>  messagesOut = messagesOut :+ "Mapping expression not a Mapping."
+//    }
+    //    t.checkInternal(fieldID)
+    //    n.definition(null)(fieldID) match {
+    //      case Some(value) => fieldID.decl = value
+    //      case None => messagesOut ++= "Field in comprehension not found."
+    //    }
     Some(messagesOut)
   }
+
+  override def typecheck(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = {
+    None
+  }
+
 
 
 
