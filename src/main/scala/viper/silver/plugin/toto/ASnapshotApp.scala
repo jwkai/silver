707d19de1491a3a085fc880f388cce0b4ef93bf7
toto <tokaeo@cs.ubc.ca>
2023-07-31 18:36:46 -0700

    Fix type of ASnapshotApp, link to ADecl

--- src/main/scala/viper/silver/plugin/toto/ASnapshotApp.scala
+++ src/main/scala/viper/silver/plugin/toto/ASnapshotApp.scala
@@ -1,36 +1,49 @@
 package viper.silver.plugin.toto
 
-import viper.silver.ast.{Declaration, ErrorTrafo, Exp, ExtensionExp, Field, Info, MapType, NoInfo, NoPosition, NoTrafos, Node, Position, Type}
-import viper.silver.ast.pretty.FastPrettyPrinter.{ContOps, text, toParenDoc}
-import viper.silver.ast.pretty.{FastPrettyPrinter, PrettyPrintPrimitives}
+import viper.silver.ast.pretty.FastPrettyPrinter.{ContOps, text}
+import viper.silver.ast.pretty.PrettyPrintPrimitives
+import viper.silver.ast._
 import viper.silver.verifier.VerificationResult
 
 case class ASnapshotApp(comprehension4Tuple: AComprehension4Tuple, filter: Exp, field: String)
                        (val pos: Position = NoPosition, val info: Info = NoInfo,
                                            val errT: ErrorTrafo = NoTrafos) extends ExtensionExp {
-  private var snapshotFunctionDeclaration : Declaration = _
 
-  def linkToSnapFunction(d: Declaration): Unit = {
-    snapshotFunctionDeclaration = d
+  var snapshotFunctionDeclaration : Declaration = {
+    val receiverType = comprehension4Tuple.tripleType
+    ASnapshotDecl.getOrMakeNewSnapDecl(receiverType, field)
   }
 
-  def getSnapFunctionDeclaration : Declaration = {
-    snapshotFunctionDeclaration
-  }
+//  def linkToSnapFunction(d: Declaration): Unit = {
+//    snapshotFunctionDeclaration = d
+//  }
+//
+//  def getSnapFunctionDeclaration : Declaration = {
+//    snapshotFunctionDeclaration
+//  }
 
   override lazy val prettyPrint: PrettyPrintPrimitives#Cont = {
     //TODO
-    text("snap_") <+> FastPrettyPrinter.show(field)
+    text("snap_") <+> field
   }
 
   // Is field considered a subnode?
-  override val extensionSubnodes: Seq[Node] = Seq(comprehension4Tuple, filter).flatten
+  override val extensionSubnodes: Seq[Node] = Seq(comprehension4Tuple, filter)
 
   override def extensionIsPure: Boolean = true
 
   // TODO
-  override def typ: Type = MapType(filter.typ.typeVariables.head,
-    comprehension4Tuple.mapping.getOrElse(comprehension4Tuple.unit).typ)
+  override def typ: Type = {
+    val filterOn = filter.typ match {
+      case c : SetType =>
+        c.elementType
+      case _ =>
+        throw new Exception("Filter must be a set")
+    }
+    MapType(filterOn, comprehension4Tuple.unit.typ)
+  }
+  //    MapType(filter.typ.typeVariables.head,
+//    comprehension4Tuple.mapping.getOrElse(comprehension4Tuple.unit).typ)
 
   // Does not get used, transform to ordinary Viper before verification
   override def verifyExtExp(): VerificationResult = {
