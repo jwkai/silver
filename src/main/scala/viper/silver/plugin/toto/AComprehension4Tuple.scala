8b8ce08d22a887f9e07529afa5f9f21040a07e10
toto <tokaeo@cs.ubc.ca>
2023-07-31 18:37:40 -0700

    Add type-3tuple to Comprehension4Tuple

--- src/main/scala/viper/silver/plugin/toto/AComprehension4Tuple.scala
+++ src/main/scala/viper/silver/plugin/toto/AComprehension4Tuple.scala
@@ -1,9 +1,8 @@
 package viper.silver.plugin.toto
 
-import viper.silver.ast.{ErrorTrafo, Exp, ExtensionExp, Field, Info, LocalVar, NoInfo, NoPosition, NoTrafos, Node, Position, Type}
 import viper.silver.ast.pretty.FastPrettyPrinter.{ContOps, text, toParenDoc}
 import viper.silver.ast.pretty.PrettyPrintPrimitives
-import viper.silver.parser.PIdnUse
+import viper.silver.ast._
 import viper.silver.verifier.VerificationResult
 
 
@@ -20,8 +19,34 @@ case class AComprehension4Tuple(receiver: Exp, mapping: Option[Exp], op: Exp, un
 
   override def typ: Type = unit.typ
 
+
+  val tripleType: (Type, Type, Type)   = {
+    val A = receiver.typ
+    val VB = mapping match {
+      case Some(m) =>
+        m.typ match {
+          case d: DomainType if d.domainName == "Mapping" =>
+            d.typVarsMap.values
+          case _ => throw new Exception("Mapping must be a mapping")
+        }
+      case None => throw new Exception("Not implemented")
+    }
+    if (VB.size != 2) {
+      throw new Exception("Mapping must be a mapping from 2 variables")
+    }
+    val V = VB.head
+    val B = VB.tail.head
+    (A, V, B)
+  }
+
   // Does not get used, transform to ordinary Viper before verification
   override def verifyExtExp(): VerificationResult = {
       throw new Exception("Not implemented");
   }
+
+//  def translateToViper : Exp = {
+//    DomainFuncApp
+//
+//  }
+
 }
