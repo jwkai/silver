59a69dbd9ab65ccc5e6fac35c7b9ba012b47cb15
toto <tokaeo@cs.ubc.ca>
2023-07-31 18:36:05 -0700

    Change ASnapshotDecl to take a Type-3Tuple

--- src/main/scala/viper/silver/plugin/toto/ASnapshotDecl.scala
+++ src/main/scala/viper/silver/plugin/toto/ASnapshotDecl.scala
@@ -1,17 +1,35 @@
 package viper.silver.plugin.toto
 
+import viper.silver.ast.pretty.FastPrettyPrinter.text
 import viper.silver.ast.pretty.PrettyPrintPrimitives
-import viper.silver.ast.{Declaration, ErrorTrafo, ExtensionMember, Info, Node, Position, Type}
+import viper.silver.ast._
 
-case class ASnapshotDecl(name: String, compType: Type, fieldID: String)(val pos : Position) extends ExtensionMember
+// Constructor should not be called directly, use getOrMakeNewSnapDecl
+case class ASnapshotDecl private (compType: (Type, Type, Type), fieldID: String)(val pos : Position = NoPosition) extends ExtensionMember
 {
+
+  def key: String = compType.toString() + "_" + fieldID
+
+  override def name: String = key
   override def extensionSubnodes: Seq[Node] = ???
-  override def prettyPrint: PrettyPrintPrimitives#Cont = ???
+  override def prettyPrint: PrettyPrintPrimitives#Cont = text(key)
 
   override def info: Info = ???
 
   override def errT: ErrorTrafo = ???
 
   override val scopedDecls: Seq[Declaration] = ???
+
+}
+
+
+object ASnapshotDecl {
+
+  private val snapshotDecls: scala.collection.mutable.Map[String, ASnapshotDecl] = scala.collection.mutable.Map()
+
+  def getOrMakeNewSnapDecl(compType: (Type, Type, Type), fieldID: String): ASnapshotDecl = {
+    val key = compType.toString() + "_" + fieldID
+    snapshotDecls.getOrElseUpdate(key, ASnapshotDecl(compType, fieldID)())
+  }
 
 }
