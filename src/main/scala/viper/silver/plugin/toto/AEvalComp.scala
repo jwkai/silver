8570a8bf29a24f23162ab206de798fba2c9f3ba3
toto <tokaeo@cs.ubc.ca>
2023-08-01 17:22:47 -0700

    Change AEvalComp inputs to comp and snap, and implement toViper

--- src/main/scala/viper/silver/plugin/toto/AEvalComp.scala
+++ src/main/scala/viper/silver/plugin/toto/AEvalComp.scala
@@ -1,22 +1,28 @@
 package viper.silver.plugin.toto
 
-import viper.silver.ast.{ErrorTrafo, Exp, ExtensionExp, Info, NoInfo, NoPosition, NoTrafos, Node, Position, Type}
-import viper.silver.ast.pretty.FastPrettyPrinter.{ContOps,text, toParenDoc}
+import viper.silver.ast.{DomainFuncApp, ErrorTrafo, Exp, ExtensionExp, Info, NoInfo, NoPosition, NoTrafos, Node, Position, Type}
+import viper.silver.ast.pretty.FastPrettyPrinter.{ContOps, text, toParenDoc}
 import viper.silver.ast.pretty.PrettyPrintPrimitives
 import viper.silver.verifier.VerificationResult
 
-case class AEvalComp(op: Exp, unit: Exp, mapping: Option[Exp], field: Any,
-                     receiver: Exp, filter: Exp)(val pos: Position = NoPosition, val info: Info = NoInfo,
+case class AEvalComp(comp: AComprehension4Tuple, snap: ASnapshotApp)(val pos: Position = NoPosition, val info: Info = NoInfo,
                                                      val errT: ErrorTrafo = NoTrafos) extends ExtensionExp {
 
-  override lazy val prettyPrint: PrettyPrintPrimitives#Cont =
-    text("comp") <+>  toParenDoc(op) <+> toParenDoc(unit) <+> toParenDoc(receiver) <+> toParenDoc(filter)
+  def toViper : Exp = {
+    DomainFuncApp("evalComp", Seq(comp.toViper, snap.toViper), Map())(
+      pos, info, comp.tripleType._3 , "Comp" , errT
+    )
+  }
 
-  override val extensionSubnodes: Seq[Node] = Seq(op, unit, receiver, filter)
+  override lazy val prettyPrint: PrettyPrintPrimitives#Cont =
+    text("evalComp") <+>  toParenDoc(comp) <+> toParenDoc(snap)
+
+  override val extensionSubnodes: Seq[Node] = Seq(comp, snap)
+
 
   override def extensionIsPure: Boolean = true
 
-  override def typ: Type = unit.typ
+  override def typ: Type = comp.typ
 
   // Does not get used, transform to ordinary Viper before verification
   override def verifyExtExp(): VerificationResult = {
