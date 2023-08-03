481600d5782ffba815cd6ca9cc89b177e92f0c40
toto <tokaeo@cs.ubc.ca>
2023-08-03 13:16:44 -0700

    EvalComp takes program to perform conversion

--- src/main/scala/viper/silver/plugin/toto/AEvalComp.scala
+++ src/main/scala/viper/silver/plugin/toto/AEvalComp.scala
@@ -1,24 +1,23 @@
 package viper.silver.plugin.toto
 
-import viper.silver.ast.{DomainFuncApp, ErrorTrafo, Exp, ExtensionExp, Info, NoInfo, NoPosition, NoTrafos, Node, Position, Type}
-import viper.silver.ast.pretty.FastPrettyPrinter.{ContOps, text, toParenDoc}
+import viper.silver.ast.{DomainFuncApp, ErrorTrafo, Exp, ExtensionExp, Info, NoInfo, NoPosition, NoTrafos, Node, Position, Program, Type}
+import viper.silver.ast.pretty.FastPrettyPrinter._
 import viper.silver.ast.pretty.PrettyPrintPrimitives
 import viper.silver.verifier.VerificationResult
 
 case class AEvalComp(comp: AComprehension4Tuple, snap: ASnapshotApp)(val pos: Position = NoPosition, val info: Info = NoInfo,
                                                      val errT: ErrorTrafo = NoTrafos) extends ExtensionExp {
 
-  def toViper : Exp = {
-    DomainFuncApp("evalComp", Seq(comp.toViper, snap.toViper), Map())(
+  def toViper(input: Program) : Exp = {
+    DomainFuncApp("evalComp", Seq(comp.toViper(input), snap.toViper(input)), Map())(
       pos, info, comp.tripleType._3 , "Comp" , errT
     )
   }
 
   override lazy val prettyPrint: PrettyPrintPrimitives#Cont =
-    text("evalComp") <+>  toParenDoc(comp) <+> toParenDoc(snap)
+    text("evalComp") <+>  parens(ssep(Seq(show(comp), show(snap)), group(char (',') <> line)))
 
   override val extensionSubnodes: Seq[Node] = Seq(comp, snap)
-
 
   override def extensionIsPure: Boolean = true
 
