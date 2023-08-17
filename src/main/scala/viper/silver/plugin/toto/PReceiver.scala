2fd7cab990cb5812da8660c178bfd36f34de2aff
toto <tokaeo@cs.ubc.ca>
2023-08-17 16:49:07 -0700

    Implement typechecking for PReceiver

--- src/main/scala/viper/silver/plugin/toto/PReceiver.scala
+++ src/main/scala/viper/silver/plugin/toto/PReceiver.scala
@@ -1,15 +1,32 @@
 package viper.silver.plugin.toto
 
 import viper.silver.ast.{Member, Position}
-import viper.silver.parser.{NameAnalyser, PExtender, PGlobalDeclaration, PIdnDef, PMember, PNode, Translator, TypeChecker}
+import viper.silver.parser._
 
-case class PReceiver(idndef: PIdnDef)
-                                  (val pos: (Position, Position)) extends PExtender with PMember with PGlobalDeclaration {
-  override val getSubnodes: Seq[PNode] = Seq(idndef)
+case class PReceiver(idndef: PIdnDef, formalArgs: Seq[PFormalArgDecl], body : PFunInline)
+                                  (val pos: (Position, Position)) extends PExtender with PSingleMember {
+
+
+  override def annotations: Seq[(String, Seq[String])] = Seq()
+
+  override val getSubnodes: Seq[PNode] = Seq(idndef) ++ formalArgs ++ Seq(body)
+
+  var typ: PType = null;
 
   override def typecheck(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = {
+    t.checkMember(this) {
+      formalArgs.foreach( a => t.check(a.typ))
+      body.typecheckReceiver(t, n)
+      typ = ComprehensionPlugin.makeDomainType("Receiver", Seq(body.args(0).typ))
+    }
     None
   }
+
+  override def typecheck(t: TypeChecker, n: NameAnalyser, expected: PType): Option[Seq[String]] = {
+    // There is no expected type. This is a declaration.
+    typecheck(t, n)
+  }
+
 
   override def translateMember(t: Translator): Member = { ???
   }
