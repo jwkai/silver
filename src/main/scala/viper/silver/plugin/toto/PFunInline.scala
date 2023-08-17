98aab2610cff094ad67ad194a4aef6bf9e0741e4
toto <tokaeo@cs.ubc.ca>
2023-08-17 16:52:04 -0700

    Implement typechecking for PReceiver in fun def

--- src/main/scala/viper/silver/plugin/toto/PFunInline.scala
+++ src/main/scala/viper/silver/plugin/toto/PFunInline.scala
@@ -1,8 +1,23 @@
 package viper.silver.plugin.toto
 
 import viper.silver.ast.Position
-import viper.silver.parser.{PExp, PExtender, PFormalArgDecl}
+import viper.silver.parser.{NameAnalyser, PExp, PExtender, PFormalArgDecl, PIntLit, PNode, PType, PTypeVar, TypeChecker, TypeHelper}
 
 case class PFunInline(args: Seq[PFormalArgDecl], body: PExp)
                      (val pos : (Position, Position)) extends PExtender {
+
+
+  override def getSubnodes(): Seq[PNode] = args ++ Seq(body)
+
+  def typecheckReceiver(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = {
+    // this calls t.checkTopTyped, which will call checkInternal, which calls the above typecheck
+    if (args.length != 1) {
+      return Some(Seq("Receiver body should have exactly one argument."))
+    }
+    args.foreach(a => t.check(a.typ))
+    t.checkTopTyped(body, Some(TypeHelper.Ref))
+    None
+  }
+
+
 }
