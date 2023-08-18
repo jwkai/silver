de71784bf3c8520ef205625bd7fb463a80558b91
toto <tokaeo@cs.ubc.ca>
2023-08-18 00:37:55 -0700

    Implement typechecking for the top-level custom declarations

--- src/main/scala/viper/silver/plugin/toto/PMapping.scala
+++ src/main/scala/viper/silver/plugin/toto/PMapping.scala
@@ -3,18 +3,20 @@ package viper.silver.plugin.toto
 import viper.silver.ast.{Member, Position}
 import viper.silver.parser._
 
-case class PFilter(idndef: PIdnDef, formalArgs: Seq[PFormalArgDecl], body: PFunInline)
+case class PMapping(idndef: PIdnDef, formalArgs: Seq[PFormalArgDecl], fBody: PFunInline)
                   (val pos: (Position, Position)) extends PExtender with PSingleMember {
-  override val getSubnodes: Seq[PNode] = Seq(idndef) ++ formalArgs ++ Seq(body)
+
+  override val getSubnodes: Seq[PNode] = Seq(idndef) ++ formalArgs ++ Seq(fBody)
 
   var typ : PType = null;
 
   override def typecheck(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = {
     t.checkMember(this) {
       formalArgs.foreach(a => t.check(a.typ))
-      body.typecheckFilter(t, n) match {
+      fBody.typecheckMapping(t, n) match {
         case out @ Some(_) => return out
-        case None => typ = ComprehensionPlugin.makeSetType(body.args(0).typ)
+        case None => typ = ComprehensionPlugin.makeDomainType(DomainsGenerator.mapDKey,
+          Seq(fBody.args(0).typ, fBody.body.typ))
       }
     }
     None
@@ -25,4 +27,6 @@ case class PFilter(idndef: PIdnDef, formalArgs: Seq[PFormalArgDecl], body: PFunI
   override def translateMemberSignature(t: Translator): Member = super.translateMemberSignature(t)
 
   override def annotations: Seq[(String, Seq[String])] = Seq()
+
+
 }
