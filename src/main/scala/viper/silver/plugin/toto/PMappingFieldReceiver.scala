45e524869dac17ff60a9067ce56ec959a754c9e1
toto <tokaeo@cs.ubc.ca>
2023-08-24 03:09:57 -0700

    Change typechecking for field in PNode

--- src/main/scala/viper/silver/plugin/toto/PMappingFieldReceiver.scala
+++ src/main/scala/viper/silver/plugin/toto/PMappingFieldReceiver.scala
@@ -8,17 +8,32 @@ case class PMappingFieldReceiver(mapping: PCall, fieldID: PIdnUse, receiver: PCa
 
   override val getSubnodes: Seq[PNode] = Seq(mapping, fieldID, receiver)
 
-  def typecheckComp(t: TypeChecker, typeUnit: PType, typeFilter: PType): Unit = {
+  def typecheckComp(t: TypeChecker, n: NameAnalyser, typeUnit: PType, typeFilter: PType): Seq[String] = {
+    var errorSeq : Seq[String] = Seq()
+
     val correctReceiverType = ComprehensionPlugin.makeDomainType("Receiver", Seq(typeFilter))
     t.checkTopTyped(receiver, Some(correctReceiverType))
-    t.checkTopTyped(fieldID, None)
+    var field = n.definition(null)(fieldID, None)
+    field match {
+      case Some(value) =>
+        value match {
+          case decl: PFieldDecl => {
+            fieldID.typ = decl.typ
+            fieldID.decl = decl
+          }
+          case _ => return errorSeq :+ "Comprehension field not declared as a field."
+        }
+      case None => return errorSeq :+ "Field not found."
+    }
+//    t.checkTopTyped(fieldID, None)
     val correctMappingType = ComprehensionPlugin.makeDomainType("Mapping",
       Seq(fieldID.typ,typeUnit))
     t.checkTopTyped(mapping, Some(correctMappingType))
+    errorSeq
   }
 
   override def typecheck(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = {
-    Some(Seq("Internal erro: typecheck should only be called from PComprehension node."))
+    Some(Seq("Internal error: typecheck should only be called from PComprehension node."))
   }
 
   def translateTo(t: Translator): (Option[Exp], String, Exp) = {
