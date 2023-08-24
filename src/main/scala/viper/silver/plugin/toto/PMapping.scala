9ce5caab1281eb7334d8ba9c6ef7c087da98ef36
toto <tokaeo@cs.ubc.ca>
2023-08-24 03:09:03 -0700

    Update the PNodes for Comprehensions components with a parent trait

--- src/main/scala/viper/silver/plugin/toto/PMapping.scala
+++ src/main/scala/viper/silver/plugin/toto/PMapping.scala
@@ -3,24 +3,40 @@ package viper.silver.plugin.toto
 import viper.silver.ast.{Member, Position}
 import viper.silver.parser._
 
-case class PMapping(idndef: PIdnDef, formalArgs: Seq[PFormalArgDecl], fBody: PFunInline)
-                  (val pos: (Position, Position)) extends PExtender with PSingleMember {
+case class PMapping(idndef: PIdnDef, formalArgs: Seq[PFormalArgDecl], body: PFunInline)
+                  (val pos: (Position, Position))
+  extends PExtender with PAnyFunction with PCompComponentDecl  {
 
-  override val getSubnodes: Seq[PNode] = Seq(idndef) ++ formalArgs ++ Seq(fBody)
+  override val getSubnodes: Seq[PNode] = Seq(idndef) ++ formalArgs ++ Seq(body)
 
-  var typ : PType = null;
+  var typToInfer: PType = null;
+
+  override def resultType(): PType = typToInfer;
 
   override def typecheck(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = {
     t.checkMember(this) {
       formalArgs.foreach(a => t.check(a.typ))
-      fBody.typecheckMapping(t, n) match {
+      body.typecheckMapping(t, n) match {
         case out @ Some(_) => return out
-        case None => typ = ComprehensionPlugin.makeDomainType(DomainsGenerator.mapDKey,
-          Seq(fBody.args(0).typ, fBody.body.typ))
+        case None => typToInfer = ComprehensionPlugin.makeDomainType(DomainsGenerator.mapDKey,
+          Seq(body.args(0).typ, body.body.typ))
       }
     }
     None
   }
+
+
+  def pViperTranslation(posTuple: (Position, Position)): PBinExp = {
+    val args1 = Seq(PCall(PIdnUse(idndef.name)(posTuple),
+      formalArgs.map(a => PIdnUse(a.idndef.name)(posTuple)))(posTuple))
+    val args2 = body.args.map(a => PIdnUse(a.idndef.name)(posTuple))
+    val lhs = PCall(PIdnUse(DomainsGenerator.mapEvalKey)(posTuple), args1 ++ args2)(posTuple)
+    val rhs = body.body
+    PBinExp(lhs, "==", rhs)(posTuple)
+  }
+
+
+
 
   override def translateMember(t: Translator): Member = { ???
   }
--- src/main/scala/viper/silver/plugin/toto/PReceiver.scala
+++ src/main/scala/viper/silver/plugin/toto/PReceiver.scala
@@ -1,28 +1,40 @@
 package viper.silver.plugin.toto
 
-import viper.silver.ast.{Member, Position}
+import viper.silver.ast.pretty.Call
+import viper.silver.ast.{AnonymousDomainAxiom, Domain, DomainFunc, DomainFuncApp, EqCmp, Forall, LocalVar, Member, Position}
 import viper.silver.parser._
 
 case class PReceiver(idndef: PIdnDef, formalArgs: Seq[PFormalArgDecl], body : PFunInline)
-                                  (val pos: (Position, Position)) extends PExtender with PSingleMember {
+                                  (val pos: (Position, Position))
+  extends PExtender with PAnyFunction with PCompComponentDecl {
 
 
   override def annotations: Seq[(String, Seq[String])] = Seq()
 
   override val getSubnodes: Seq[PNode] = Seq(idndef) ++ formalArgs ++ Seq(body)
 
-  var typ: PType = null;
 
   override def typecheck(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = {
     t.checkMember(this) {
       formalArgs.foreach( a => t.check(a.typ))
       body.typecheckReceiver(t, n) match {
         case out @ Some(_) => return out
-        case None => typ = ComprehensionPlugin.makeDomainType(DomainsGenerator.recDKey, Seq(body.args(0).typ))
+        case None => typToInfer = ComprehensionPlugin.makeDomainType(DomainsGenerator.recDKey,
+          Seq(body.args(0).typ))
       }
     }
     None
   }
+
+  def pViperTranslation(posTuple: (Position, Position)): PBinExp = {
+    val args1 = Seq(PCall(PIdnUse(idndef.name)(posTuple),
+      formalArgs.map(a => PIdnUse(a.idndef.name)(posTuple)))(posTuple))
+    val args2 = body.args.map(a => PIdnUse(a.idndef.name)(posTuple))
+    val lhs = PCall(PIdnUse(DomainsGenerator.recEvalKey)(posTuple), args1 ++ args2)(posTuple)
+    val rhs = body.body
+    PBinExp(lhs, "==", rhs)(posTuple)
+  }
+
 
   override def typecheck(t: TypeChecker, n: NameAnalyser, expected: PType): Option[Seq[String]] = {
     // There is no expected type. This is a declaration.
@@ -30,8 +42,27 @@ case class PReceiver(idndef: PIdnDef, formalArgs: Seq[PFormalArgDecl], body : PF
   }
 
 
-  override def translateMember(t: Translator): Member = { ???
+  override def translateMember(t: Translator): Member = {
+    // Gets the dummy domain
+    val d = t.getMembers()(genDomainName).asInstanceOf[Domain]
+    // Gets the evalRec function
+    val evalFunc = t.getMembers()(DomainsGenerator.recEvalKey).asInstanceOf[DomainFunc]
+    val (funct, axiom) = getEvalFuncAxiom(d, evalFunc, t)
+    val dd = d.copy(
+      functions = d.functions :+ funct,
+      axioms = d.axioms :+ axiom
+    )(d.pos, d.info, d.errT)
+    t.getMembers()(genDomainName) = dd
+    dd
+
   }
-  override def translateMemberSignature(t: Translator): Member = super.translateMemberSignature(t)
+//  override def translateMemberSignature(t: Translator): Member = {
+////    val pospos: Position = PDomain(null, null, null, null, null)(null, null)
+//    Domain(name = genDomainName, functions = Seq(), axioms = Seq())(
+//      pos = t.liftPos(this), info = t.toInfo(this.annotations, this)
+//    )
+//  }
+
+
 
 }
