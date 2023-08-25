f584ca516eb723e708e95599359d3c1db79f345a
toto <tokaeo@cs.ubc.ca>
2023-08-25 00:43:19 -0700

    Implement conversion from comprehension declarations to Domains

--- src/main/scala/viper/silver/plugin/toto/PFilter.scala
+++ src/main/scala/viper/silver/plugin/toto/PFilter.scala
@@ -6,10 +6,10 @@ import viper.silver.parser._
 case class PFilter(idndef: PIdnDef, formalArgs: Seq[PFormalArgDecl], body: PFunInline)
                   (val pos: (Position, Position))
   extends PExtender with PAnyFunction with PCompComponentDecl {
-  override val getSubnodes: Seq[PNode] = Seq(idndef) ++ formalArgs ++ Seq(body)
+//  override val getSubnodes: Seq[PNode] = Seq(idndef) ++ formalArgs ++ Seq(body)
 
-  var typToInfer: PType = null;
-  override def resultType(): PType = typToInfer;
+//  var typToInfer: PType = null;
+//  override def resultType(): PType = typToInfer;
 
   override def typecheck(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = {
     t.checkMember(this) {
@@ -22,20 +22,17 @@ case class PFilter(idndef: PIdnDef, formalArgs: Seq[PFormalArgDecl], body: PFunI
     None
   }
 
-  def pViperTranslation(posTuple: (Position, Position)): PBinExp = {
-    val elem = PIdnUse(body.args(0).idndef.name)(posTuple)
-    val set = PCall(PIdnUse(idndef.name)(posTuple),
-      formalArgs.map(a => PIdnUse(a.idndef.name)(posTuple)))(posTuple)
+//  def pViperTranslation(posTuple: (Position, Position)): PBinExp = {
+//    val elem = PIdnUse(body.args(0).idndef.name)(posTuple)
+//    val set = PCall(PIdnUse(idndef.name)(posTuple),
+//      formalArgs.map(a => PIdnUse(a.idndef.name)(posTuple)))(posTuple)
+//
+//    val lhs = PBinExp(elem, "in", set)(posTuple)
+//    val rhs = body.body
+//    PBinExp(lhs, "==", rhs)(posTuple)
+//  }
 
-    val lhs = PBinExp(elem, "in", set)(posTuple)
-    val rhs = body.body
-    PBinExp(lhs, "==", rhs)(posTuple)
+  override def translateMember(t: Translator): Member = {
+    translateMemberWithName(t, None)
   }
-
-
-  override def translateMember(t: Translator): Member = { ???
-  }
-  override def translateMemberSignature(t: Translator): Member = super.translateMemberSignature(t)
-
-  override def annotations: Seq[(String, Seq[String])] = Seq()
 }
