03edc1da3c060a4a3f8c79ea101541a84da0f0bc
toto <tokaeo@cs.ubc.ca>
2023-10-12 15:04:23 -0700

    Add way to extract comp domainType from SnapshotDecl ast node

--- src/main/scala/viper/silver/plugin/toto/ASnapshotDecl.scala
+++ src/main/scala/viper/silver/plugin/toto/ASnapshotDecl.scala
@@ -6,10 +6,10 @@ import viper.silver.ast._
 import viper.silver.plugin.toto.ASnapshotDecl.tupleFieldToString
 
 // Constructor should not be called directly, use getOrMakeNewSnapDecl
-case class ASnapshotDecl private (compType: (Type, Type, Type), fieldID: String)(val pos : Position = NoPosition)
+case class ASnapshotDecl private(compType: (Type, Type, Type), fieldName: String)(val pos : Position = NoPosition)
   extends ExtensionMember
 {
-  def key: String = tupleFieldToString(compType, fieldID)
+  def key: String = tupleFieldToString(compType, fieldName)
 
   override def name: String = key
   override def extensionSubnodes: Seq[Node] = ???
@@ -24,6 +24,21 @@ case class ASnapshotDecl private (compType: (Type, Type, Type), fieldID: String)
   val outType: Type = {
     MapType(compType._1, compType._3)
   }
+
+  def compDType(input: Program) = {
+    val compDomain = input.findDomain(DomainsGenerator.compDKey)
+    val typeVars = compDomain.typVars
+    if (typeVars.length != 3) {
+      throw new Exception("Comp domain must have 3 type variables")
+    }
+    val typeVarMap = Map(
+      typeVars(0) -> compType._1,
+      typeVars(1) -> compType._2,
+      typeVars(2) -> compType._3
+    )
+    DomainType.apply(compDomain, typeVarMap)
+  }
+
 
   def viperDecl(input: Program): Function = {
     val compDomain = input.findDomain(DomainsGenerator.compDKey)
@@ -43,6 +58,9 @@ case class ASnapshotDecl private (compType: (Type, Type, Type), fieldID: String)
     Function(name, Seq(args0, args1), outType, Seq(), Seq(), None)()
   }
 
+  def findFieldInProgram(p: Program): Field = {
+    p.findField(fieldName)
+  }
 
 
 }
@@ -62,7 +80,7 @@ object ASnapshotDecl {
   }
 
   def tupleFieldToString(t: (Type, Type, Type), fieldID: String): String = {
-    "snap_" + t._1.toString() + "_" + t._2.toString() + "_" + t._3.toString() + "_" + fieldID
+    "__snap_" + t._1.toString() + "_" + t._2.toString() + "_" + t._3.toString() + "_" + fieldID
   }
 
 
